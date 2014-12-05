c6dd16534289061a6d47046008b42105e1d01482
sclaassens.ad@gmail.com <sclaassens.ad@gmail.com>
2014-12-05 09:18:39 -0800

    Updated and merged.

--- arnerve/core/LCMManager.py
+++ arnerve/core/LCMManager.py
@@ -5,17 +5,28 @@ Created on Sep 7, 2014
 '''
 import lcm
 
+#Import the user types
 from user_update_t import user_update_t
-import role_response_t
+
+#Import the bot types
+from bot_update_t import bot_update_t
+from bot_control_command_t import bot_control_command_t
+
+#Import the role types
+from role_response_t import role_response_t
 
 class LCMManager():
     
     def __init__(self):
         
-        self.lc = lcm.LCM()
+        #Broadcast across the wire
+        self.lc = lcm.LCM("udpm://239.255.76.67:7667?ttl=1")
         self.__subscriptions = []
         self.__subscriptions.append(self.lc.subscribe("ARNerve_UserUpdates", self.UpdateUsersHandler))
         self.__subscriptions.append(self.lc.subscribe("ARNerve_UserHerder_RoleResponses", self.UpdateFromRoleResponse))
+        
+        # Add all the bot channels.
+        self.__subscriptions.append(self.lc.subscribe("ARNerve_Bot_Update_GIRR", self.UpdateBot))
 
     def Attach(self, userManager, roleManager):
         '''
@@ -31,6 +42,29 @@ class LCMManager():
         msg = user_update_t.decode(data)
         if(self.__userManager):
             self.__userManager.UpdateUserFromLCM(msg)
+            
+        #HACK TESTING...
+        botControl = bot_control_command_t()
+        botControl.name = "GIRR"
+        botControl.botTreadVelLeft = 0
+        botControl.botTreadVelLeft = 0
+        if msg.kinect.is_lhandclosed and msg.kinect.is_rhandclosed:
+            botControl.botTreadVelLeft = 1.0
+            botControl.botTreadVelright = 1.0
+        else:
+            if msg.kinect.is_lhandclosed:
+                print "---Left Hand CLosed!"
+                botControl.botTreadVelLeft = 1.0
+                botControl.botTreadVelright = -1.0
+            if msg.kinect.is_rhandclosed:
+                print "---Right Hand CLosed!"
+                botControl.botTreadVelLeft = -1.0
+                botControl.botTreadVelright = 1.0
+        botControl.isInfraredOn = 0
+        botControl.isLightsOn = 0
+        botControl.timestamp = 0
+        self.lc.publish("ARNerve_Bot_Control_GIRR", botControl.encode())
+        
     
     def UpdateFromRoleResponse(self, channel, data):
         '''
@@ -40,6 +74,14 @@ class LCMManager():
         
         # Now pass it to the role manager
         self.__roleManager.ParseRoleResponse(roleResponse)
+        
+    def UpdateBot(self, channel, data):
+        '''
+        Update from a bot frame
+        '''
+        botUpdate = bot_update_t.decode(data)
+        print "[LCMManager] Got an update for bot {0}".format(botUpdate.name)
+        return
           
     def Update(self):
         self.lc.handle()
